4cb97afb99e886017c8ee36d35f6892f194c57ff
Rob Newsom <rwnewsom@gmail.com>
2021-08-02 19:15:48 -0400

    added link etc

--- vue/src/services/BreweryService.js
+++ vue/src/services/BreweryService.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+// Configuration options for Axios - Added 02AUG2021
+let options = {
+  baseURL: 'https://localhost:44315/Brewery'
+}
+//Also added 02AUG2021
+let client = axios.create(options); // Creating the equivalent of a RestClient in RestSharp
+
 export default {
     list() {
-        return axios.get('/breweries');
+        return client.get('/breweries');
       },
 }
