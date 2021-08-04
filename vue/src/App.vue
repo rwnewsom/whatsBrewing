e2f5273f102625d21ff8865f1774e3fc0265f819
Sally Wood <sally.wood66@gmail.com>
2021-08-03 22:32:04 -0400

    added grids and styling to the brewery list

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,63 +2,74 @@
      Note that you have classes from bootstrap available to you.
      See https://getbootstrap.com/docs/4.5/getting-started/introduction/ for reference on bootstrap -->
 <template>
-  <div id="app" class="container"> <!-- If you start to get random styling you don't like, remove container from this div -->
+  <div id="app">
+    <!-- If you start to get random styling you don't like, remove container from this div -->
     <div id="nav">
-      <router-link class="nav-item" v-bind:to="{ name: 'home' }">
-        <i class="fas fa-home"></i> <!-- This is a font awesome icon -->
-        Home 
-      </router-link>
+      <div id="nav-spacer" />
+        <router-link class="nav-item home" v-bind:to="{ name: 'home' }">
+          <i class="fas fa-home"></i>
+          <!-- This is a font awesome icon -->
+          Home
+        </router-link>
 
-      <router-link class="nav-item" v-bind:to="{ name: 'breweries' }">
-        <i class="fas fa-beer"></i> <!-- This is a font awesome icon -->
-        All Breweries 
-      </router-link>
+        <router-link class="nav-item breweries" v-bind:to="{ name: 'breweries' }">
+          <i class="fas fa-beer"></i>
+          <!-- This is a font awesome icon -->
+          All Breweries
+        </router-link>
 
-      <router-link
-        class="nav-item"
-        v-bind:to="{ name: 'register' }"
-        v-if="!$store.state.token">&nbsp;|&nbsp;Register</router-link>
-      <router-link
-        class="nav-item"
-        v-bind:to="{ name: 'login' }"
-        v-if="!$store.state.token">
-        &nbsp;|&nbsp;Login
-      </router-link>
-      <router-link
-        class="nav-item"
-        v-bind:to="{ name: 'logout' }"
-        v-if="$store.state.token">
-        &nbsp;|&nbsp;Logout
-      </router-link>
+        <router-link
+          class="nav-item register"
+          v-bind:to="{ name: 'register' }"
+          v-if="!$store.state.token"
+        >
+          Register
+        </router-link>
+        <router-link
+          class="nav-item login"
+          v-bind:to="{ name: 'login' }"
+          v-if="!$store.state.token"
+        >
+          Login
+        </router-link>
+        <router-link
+          class="nav-item login"
+          v-bind:to="{ name: 'logout' }"
+          v-if="$store.state.token"
+        >
+          Logout
+        </router-link>
     </div>
 
     <div class="main">
       <router-view />
     </div>
 
-
     <div class="ad">
-      
-      <iframe width="224" height="126" src="https://www.youtube.com/embed/gtbgKR2OLBc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe
+        width="224"
+        height="126"
+        src="https://www.youtube.com/embed/gtbgKR2OLBc"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
-    
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <!-- Application-Wide Styles go here. 
      Any valid CSS or SCSS is allowable here. See https://sass-lang.com/documentation/style-rules for info on SCSS -->
 <style lang="scss">
-
-@import './styles/colors.scss';
+@import "./styles/colors.scss";
 
 // Import the bootstrap styles. If you want to override Bootstrap colors, it needs to happen above this line
-@import '../node_modules/bootstrap/scss/bootstrap';
+@import "../node_modules/bootstrap/scss/bootstrap";
 
 // Your custom styles go below this point
 
@@ -69,44 +80,30 @@ html {
   padding: 0; */
 }
 
-#nav{
-  grid-area: head;
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  background-color: $magenta;
-}
-
-.nav-item{
-  color: $accentLight;
-  font-size: 1.5rem;
-}
-
 #app {
   background-color: $background;
   color: $accentLight;
 
   display: grid;
-  grid-template-areas: "head head"
-                       "main ad";
-  grid-template-columns:4fr 1fr;
-  grid-template-rows:   3rem 1fr;
+  grid-template-areas:
+    "head head"
+    "main ad";
+  grid-template-columns: 4fr 1fr;
+  grid-template-rows: 10rem 1fr;
 
   height: 100vh;
   width: 100vw;
-
 }
 
-.main{
+.main {
   grid-area: main;
 }
 
-
-.ad{
+.ad {
   grid-area: ad;
   display: flex;
   justify-content: space-around;
+  background-color: $babypowder;
 }
 
 .ad iframe {
@@ -114,9 +111,53 @@ html {
 }
 
 //override default body & html padding & margin to eliminate scrollbar
-body{
+body {
   margin: 0;
   padding: 0;
 }
 
+#nav {
+  grid-area: head;
+  display: grid;
+  grid-template-areas:
+    "spacer spacer spacer spacer"
+    "home breweries register login";
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 2fr 1fr;
+
+  width: 100%;
+  text-align: center; 
+  align-items: center;
+}
+
+#nav-spacer {
+  grid-area: spacer;
+  width: 100%;
+  height: 100%;
+  background-color: #E71D36;
+}
+
+.nav-item {
+  background-color: #2ec4b6;
+  color: $accentLight;
+  font-size: 1.5rem;
+  height: 100%;
+  text-transform: uppercase;
+}
+
+.home {
+  grid-area: home;
+}
+
+.breweries {
+  grid-area: breweries;
+}
+
+.register {
+  grid-area: register;
+}
+
+.login {
+  grid-area: login;
+}
 </style>
--- vue/src/views/DisplayBreweries.vue
+++ vue/src/views/DisplayBreweries.vue
@@ -1,134 +1,99 @@
 <template>
   <div class ="brew-display">
-     
-       <div class="brewery">
-    <table>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Description</th>
-        </tr>
-      </thead>
-      <tbody>
-        <!-- Changed to computed property, of as opposed to in -->
-        <tr v-for="brewery of allBreweries" v-bind:key="brewery.id">
-          <td width="10%">
-              {{brewery.id}}
-          </td>
-          <td>
-            <router-link v-bind:to="{name: 'breweryDetails', params: {id: brewery.id}}">
-              {{brewery.name}}
-              </router-link>
-              
-          </td>
-          <td>
-            {{brewery.description}}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div id="breweries">
+      <!-- all the breweries show on the line below-->
+      <div class="brewery-card" v-for="brewery of allBreweries" v-bind:key="brewery.id">
+        <img class="brewery-card-img" src="../assets/proriat-hospitality-unsplash.jpg" />
+        <div class="brewery-card-title">
+          <router-link v-bind:to="{name: 'breweryDetails', params: {id: brewery.id}}">
+            {{brewery.name}}
+          </router-link>
+        </div>
+      </div>
     </div>
-  <div class = "filt">
-    <p>filter</p>
-  </div>
+    <div class = "filt">
+      <p>filter</p>
+    </div>
   </div>
 </template>
 
 <script>
-import BreweryService from '../services/BreweryService.js'
+import BreweryService from "../services/BreweryService.js";
 
 export default {
-
   computed: {
-      allBreweries(){
-        return this.$store.state.breweries;
-      }
+    allBreweries() {
+      return this.$store.state.breweries;
     },
+  },
 
-    created() {
-      console.log('Attempting brewery request...'); //this is the last loggged message
+  created() {
+    console.log("Attempting brewery request..."); //this is the last loggged message
 
+    BreweryService.list().then((result) => {
+      console.log("Promise Resolved", result);
 
-      BreweryService.list()
-      .then(result => {
-        console.log('Promise Resolved', result);
-
-
-        if(result.status === 200) {
-          this.$store.commit('LOADED_BREWERIES', result.data);
-        }
-      } );
-    }
- 
-}
+      if (result.status === 200) {
+        this.$store.commit("LOADED_BREWERIES", result.data);
+      }
+    });
+  },
+};
 </script>
 
 
 <style lang="scss">
+@import "../styles/colors.scss";
 
-@import '../styles/colors.scss';
-
-.brew-display{
+.brew-display {
   display: grid;
-    grid-template-areas: "filt main";                       
-    grid-template-columns: 1fr 4fr;
+  grid-template-areas: "filt main";
+  grid-template-columns: 1fr 4fr;
 }
 
-.filt{
+.filt {
   grid-area: filt;
   vertical-align: text-top;
+  background-color: $babypowder;
 }
 
-table {
-    grid-area: main;
-    width: 100%;
-}
-
-table thead th {
-    text-align: left;
-}
-
-
-
-table {
-  margin-top: 1rem;
-  border: 1px solid $accentLight;
-  width: 100%;
-  border-collapse: collapse;
-}
-table td, table th {
-  border: 1px solid $accentLight;
-  padding: 3px 2px;
-}
-table tbody td {
-  font-size: 13px;
-}
-
-tr:nth-child(odd) a{
-  color: whitesmoke;
-}
-
-table tr:nth-child(even) {
-  background: $accentLight;
-  color: $accentDark;
-}
-
-
-table thead {
-  background: $accentDark;
-  color: $accentLight;
-  border-bottom: 5px solid $accentLight;
-}
-table thead th {
-  font-size: 17px;
+.brewery-card {
+  color: #011627;
+  background: $babypowder;
+  text-align: center;
+  width: 65%;
+  height: 355px;
+  margin-left: 17.5%;
+  margin-right: 17.5%;
+  margin-top: 2rem;
+  font-size: 2rem;
   font-weight: bold;
-  color: $accentLight;
-  text-align: left;
-  border-left: 2px solid $accentLight;
-}
-table thead th:first-child {
-  border-left: none;
+  font-family: sans-serif;
+  border-radius: 20px;
 }
 
+.brewery-card-title {
+  text-transform: uppercase;
+}
+
+.brewery-card-title > a {
+  text-decoration-line: none;
+}
+.brewery-card-title > a:hover {
+  text-decoration-line: underline;
+}
+
+#breweries {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.brewery-card-img {
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+}
 </style>
