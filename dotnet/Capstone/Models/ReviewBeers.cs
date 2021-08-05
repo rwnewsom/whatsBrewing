649ab142754eff3ca13e5b5e9a62dfb20af2f9f7
Jamie Mullins <janetzkej@gmail.com>
2021-08-05 12:27:34 -0400

    why isn't it working

--- dotnet/Capstone/Models/ReviewBeers.cs
+++ dotnet/Capstone/Models/ReviewBeers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace Capstone.Models
 {
     public class ReviewBeers
     {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; } = 1;
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int BeerId { get; set; }
     }
 }
