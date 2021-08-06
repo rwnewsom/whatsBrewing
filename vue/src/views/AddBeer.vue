3e81d71f71b351d2eb102f76647a6367f0c4eefc
Rob Newsom <rwnewsom@gmail.com>
2021-08-06 14:48:16 -0400

    form hiding

--- vue/src/views/AddBeer.vue
+++ vue/src/views/AddBeer.vue
@@ -45,7 +45,7 @@
                     <textarea class="form-control" id="description" rows="3"
                             v-model.trim="newBeer.description" placeholder="Description"></textarea>
                 </div>
-                <button type="submit" class="btn btn-success" 
+                <button type="submit" class="btn btn-success" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"
                         v-bind:disabled="isSaving">
                     <span v-if="isSaving" 
                         class="spinner-border spinner-border-sm" 
