219ecbd3a217f52d33f17e0f57ee40108c4ff0b5
Rob Newsom <rwnewsom@gmail.com>
2021-08-08 16:05:00 -0400

    styled filter options and made responsive

--- vue/src/components/DisplayAllBeers.vue
+++ vue/src/components/DisplayAllBeers.vue
@@ -1,19 +1,32 @@
 <template>
 <div class="beer-display">
 
-    <div class="search-field">
-        <span>
-            <label for="searchBrewery">Search Brewery</label>
-            <input id="searchBrewery" type="text" v-model="searchBrewery">
-        </span>
-        <span>
-            <label for="searchName">Search Name</label>
-            <input id="searchName" type="text" v-model="searchName">
-        </span>
-        <span>
-            <label for="searchStyle">Search by Style</label>
-            <input id="searchStyle" type="text" v-model="searchStyle">
-        </span>
+    <div class="container">
+        <h3>Search by:</h3>
+        <div class="row">
+            <div class="col-25">
+                <label for="searchBrewery">Brewery: </label>
+            </div>
+            <div class="col-75">
+                <input id="searchBrewery" type="text" v-model="searchBrewery">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+                <label for="searchName">Beer Name: </label>
+            </div>
+            <div class="col-75">
+                <input id="searchName" type="text" v-model="searchName">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+                <label for="searchStyle">Beer Style: </label>
+            </div>
+            <div class="col-75">
+                <input id="searchStyle" type="text" v-model="searchStyle">
+            </div>
+        </div>
 
 
     </div> 
@@ -103,6 +116,54 @@ tr:nth-child(even){
     border: 1px solid $yellow;
     width: 80%;
     padding: 2rem;
+}
+
+
+
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+.col-25 {
+  float: left;
+  width: 25%;
+  margin-top: 6px;
+}
+
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+}
+
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+
+input[type=text] {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .col-25, .col-75, input[type=submit] {
+    width: 100%;
+    margin-top: 0;
+  }
 }
 
 </style>
