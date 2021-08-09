0023e85e8d4d2b68bbefd708bed3d44f58ba5dbe
Rob Newsom <rwnewsom@gmail.com>
2021-08-08 21:10:51 -0400

    maps are in

--- vue/src/views/BreweryDetails.vue
+++ vue/src/views/BreweryDetails.vue
@@ -2,8 +2,8 @@
     <div id="brewery-details">
         <div class="brewery">
             <div class="breweryDetails">
-                <h1 class="name">{{currentBrewery.name}}</h1>
-                <h3 class="description">{{currentBrewery.description}}</h3>
+                <h2 class="name">{{currentBrewery.name}}</h2>
+                <h4 class="description">{{currentBrewery.description}}</h4>
                 <div class="address">
                     <p>Street Address:</p>
                     <p>{{currentBrewery.streetNumber}} {{currentBrewery.streetName}}</p>
@@ -11,7 +11,12 @@
                     <p>{{currentBrewery.phoneNumber}}</p>
                 </div>
                 <p class="url"><a v-bind:href="currentBrewery.url" > Visit Website for {{currentBrewery.name}} </a></p>
+                
+                <div class="map" v-if="renderMap">
+                        <iframe v-bind:src="currentBrewery.mapUrl" width="300" height="225" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
             </div>
+
             
             <div class="beerDetails">
                 <h4>Current Beers</h4>
@@ -49,9 +54,14 @@ export default {
             return this.$store.state.breweries;
         },
 
-        currentMapUrl(){
-            return '"' +this.currentBrewery.mapUrl+'"';
+        renderMap(){
+            if(this.currentBrewery.mapUrl){
+                return true;
+            }
+            return false;
         },
+
+        
         deleteOrAddAuth(){
             if (this.$store.state.user.role == "admin" ||this.$store.state.user.role == "brewer"){
                 return true;
@@ -89,14 +99,22 @@ export default {
     grid-template-areas: 
         "name name name "
         "dsc dsc dsc"
-        "adr . ."
+        "adr . map"
         "url url url";
+    grid-template-rows: max-content;
 }
 
 .address{
     grid-area: adr;
     padding-left: 1rem;
     
+}
+
+.map{
+    grid-area: map;
+    border: 1px solid $black;
+    border-radius: 5px;
+    filter: drop-shadow(0.25rem 0.1rem 0.5rem $blue);
 }
 
 .address p{
@@ -135,6 +153,8 @@ export default {
     grid-template-columns: 4fr 1fr;
     height: 100%;
 }
+
+
 
 .brewery {
     grid-area: details;
