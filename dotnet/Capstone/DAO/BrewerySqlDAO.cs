10eca8ba401c3e5167dcad0dc997f9de3a922b82
Quintin Owsley <quintinowsley@gmail.com>
2021-08-09 16:32:08 -0400

    Update brewery working on server side

--- dotnet/Capstone/DAO/BrewerySqlDAO.cs
+++ dotnet/Capstone/DAO/BrewerySqlDAO.cs
@@ -13,8 +13,11 @@ namespace Capstone.DAO
 
         private string sqlListBreweries = "SELECT brewery_id, brewery_name, brewery_description FROM breweries";
 
-        private string sqlBreweryDetails = "SELECT br.brewery_name, br.brewery_description, a.street_number, a.street_name, a.city_name, a.state, a.zip_code, a.phone_number, a.url, a.map_url " +
+        private string sqlBreweryDetails = "SELECT br.brewery_id, br.brewery_name, br.brewery_description, a.street_number, a.street_name, a.city_name, a.state, a.zip_code, a.phone_number, a.url, a.map_url " +
                                             "FROM breweries br INNER JOIN address a ON br.brewery_id = a.brewery WHERE br.brewery_id = @id"; //TODO: Parameter
+
+        private string sqlAddBrewery = "INSERT INTO breweries (brewery_name, brewery_description) VALUES (@brewery_name, @brewery_description)";
+        private string sqlUpdateBrewery = "BEGIN TRANSACTION UPDATE breweries SET brewery_name = @brewery_name, brewery_description = @brewery_description WHERE brewery_id = @brewery_id UPDATE address SET street_number = @street_number, street_name = @street_name, city_name = @city_name, state = @state, zip_code = @zip_code, phone_number = @phone_number, map_url = @map_url, url = @url WHERE brewery = @brewery_id COMMIT TRANSACTION";
         public BrewerySqlDAO(string dbConnectionString)
         {
             connectionString = dbConnectionString;
@@ -48,7 +51,7 @@ namespace Capstone.DAO
         {
             BreweryDetails breweryDetails = new BreweryDetails();
 
-            using(SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
                 SqlCommand cmd = new SqlCommand(sqlBreweryDetails, conn);
@@ -59,6 +62,7 @@ namespace Capstone.DAO
                 {
                     while (reader.Read())
                     {// If error verify if alias needed
+                        breweryDetails.Id = Convert.ToInt32(reader["brewery_id"]);
                         breweryDetails.Name = Convert.ToString(reader["brewery_name"]);
                         breweryDetails.Description = Convert.ToString(reader["brewery_description"]);
                         breweryDetails.StreetNumber = Convert.ToInt32(reader["street_number"]);
@@ -68,9 +72,9 @@ namespace Capstone.DAO
                         breweryDetails.ZipCode = Convert.ToInt32(reader["zip_code"]);
                         breweryDetails.PhoneNumber = Convert.ToString(reader["phone_number"]);
                         breweryDetails.Url = Convert.ToString(reader["url"]);
-                        if(reader["map_url"] != DBNull.Value)
+                        if (reader["map_url"] != DBNull.Value)
                         {
-                        breweryDetails.MapUrl = Convert.ToString(reader["map_url"]);
+                            breweryDetails.MapUrl = Convert.ToString(reader["map_url"]);
                         }
                     }
 
@@ -79,7 +83,7 @@ namespace Capstone.DAO
 
                 return null;
             }
-            
+
 
         }
 
@@ -87,7 +91,6 @@ namespace Capstone.DAO
         {
             Brewery brewery = new Brewery()
             {
-                Id = Convert.ToInt32(reader["brewery_id"]),
                 Name = Convert.ToString(reader["brewery_name"]),
                 Description = Convert.ToString(reader["brewery_description"]),
             };
@@ -102,18 +105,40 @@ namespace Capstone.DAO
             {
                 conn.Open();
 
-                SqlCommand cmd = new SqlCommand(, conn);
-                cmd.Parameters.AddWithValue("@name", beer.Name);
-                cmd.Parameters.AddWithValue("@style", beer.Style);
-                cmd.Parameters.AddWithValue("@description", beer.Description);
-                cmd.Parameters.AddWithValue("@ABV", beer.ABV);
-                cmd.Parameters.AddWithValue("@IBU", beer.IBU);
-                cmd.Parameters.AddWithValue("@brewery", beer.BreweryId);
+                SqlCommand cmd = new SqlCommand(sqlAddBrewery, conn);
+                cmd.Parameters.AddWithValue("@nbrewery_name", brewery.Name);
+                cmd.Parameters.AddWithValue("@brewery_description", brewery.Description);
                 int added = cmd.ExecuteNonQuery();
                 return added == 1;
             }
 
 
+        }
+
+        public BreweryDetails UpdateBrewery(BreweryDetails brewery)
+        {
+            using (SqlConnection conn = new SqlConnection(this.connectionString))
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(sqlUpdateBrewery, conn);
+                command.Parameters.AddWithValue("@brewery_name", brewery.Name);
+                command.Parameters.AddWithValue("@brewery_description", brewery.Description);
+                command.Parameters.AddWithValue("@brewery_id", brewery.Id);
+                command.Parameters.AddWithValue("@street_number", brewery.StreetNumber);
+                command.Parameters.AddWithValue("@street_name", brewery.StreetName);
+                command.Parameters.AddWithValue("@city_name", brewery.CityName);
+                command.Parameters.AddWithValue("@state", brewery.State);
+                command.Parameters.AddWithValue("@zip_code", brewery.ZipCode);
+                command.Parameters.AddWithValue("@phone_number", brewery.PhoneNumber);
+                command.Parameters.AddWithValue("@map_url", brewery.MapUrl);
+                command.Parameters.AddWithValue("@url", brewery.Url);
+
+                command.ExecuteNonQuery();
+            }
+
+            BreweryDetails updatedBrewery = GetBreweryById(brewery.Id);
+            return updatedBrewery;
         }
     }
 }
--- dotnet/Capstone/Models/BreweryDetails.cs
+++ dotnet/Capstone/Models/BreweryDetails.cs
@@ -12,6 +12,7 @@ namespace Capstone.Models
         /// Id represents primary key of database
         /// </summary>
         
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int StreetNumber { get; set; }
