2afdfdfe63db7c4722929daf48aa7f60b0ec632c
Quintin Owsley <quintinowsley@gmail.com>
2021-08-12 10:44:46 -0400

    added ads

--- vue/src/components/Ad.vue
+++ vue/src/components/Ad.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="ad">
-      <img class="ad-img" src="https://s3-prod.adage.com/s3fs-public/beer-cases-sold-sidebar.jpg" />
+      <img class="ad-img" src="../assets/BetterCallSaul.jpg">
+      <img class="ad-img" src="https://i.pinimg.com/originals/f7/fe/39/f7fe39708b9d1b585304dcea08197719.jpg">
       <img class="ad-img-2" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/16d9dc86194045.5d9250eb67932.jpg" />
+      <img class="ad-img" src="https://s3-prod.adage.com/s3fs-public/beer-cases-sold-sidebar.jpg" />
+      <img class="ad-img" src="https://www.americancraftbeer.com/wp-content/uploads/2018/12/Coors-Light-min.png">
     </div>
 </template>
 
@@ -44,6 +47,14 @@
   border-radius: 5px;
   display: flex;
   grid-area: ad2
+}
+.ad-img-3 {
+  width: 91%;
+  height: fit-content;
+  border: 5px solid $black;
+  margin-top: 2rem;
+  border-radius: 5px;
+  display: flex;
 }
 
 </style>
