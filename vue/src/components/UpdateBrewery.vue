040e2382d13590935351933733fa4503d8182619
Quintin Owsley <quintinowsley@gmail.com>
2021-08-12 15:20:56 -0400

    added brewery logos

--- vue/src/components/UpdateBrewery.vue
+++ vue/src/components/UpdateBrewery.vue
@@ -165,7 +165,8 @@ export default {
                         phoneNumber: '',
                         url: '',
                         imageUrl: '',
-                        mapUrl: '',  
+                        mapUrl: '', 
+                        logo: '' 
                     }
                 }
             })
